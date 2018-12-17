a2b4c06c3972d73cf1a4090a63b10a96598ba068
Roberto <roberto.carreon@cceo.com.mx>
2018-12-17 14:26:25 -0600

    Controladores

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    //c
+
+    public function index()
+    {
+        return 'Usuarios';
+    }
+    public function show($id)
+    {
+        return "Mostrando detalle del usuario: {$id}";
+    }
+    public function create()
+    {
+        return 'Crear nuevo usuario';
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -6,19 +6,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/usuarios', function () {
-    return 'Usuarios';
-});
-Route::get('/usuarios/nuevo', function () {
-    return 'Crear nuevo usuario';
-});
-Route::get('/usuarios/{id}', function ($id) {
-    return "Mostrando detalle del usuario: {$id}";
-});
-Route::get('/saludo/{name}/{nickname?}', function ($name, $nickname = null) {
-    if ($nickname) {
-        return "Bienvenido {$name}, tu apodo es {$nickname}";
-    } else {
-        return "Bienvenido {$name}, no tienes apodo";
-    }
-});
+Route::get('/usuarios', 'UserController@index');
+Route::get('/usuarios/{id}', 'UserController@show')
+    ->where('id', '[0-9]+');
+Route::get('/usuarios/nuevo', 'UserController@create');
+Route::get('/saludo/{name}/{nickname?}', 'WelcomeUserController');
