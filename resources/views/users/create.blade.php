7edcb2bd398d6b34d78e4d6432a1bfdf560280df
Roberto <roberto.carreon@cceo.com.mx>
2018-12-26 13:26:06 -0600

    Bootstrap para formulario

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -3,47 +3,46 @@
 
 @section('title', "Crear usuario")
 @section('content')
-        <h1>Crear usuario</h1>
-        <hr>
 
-        @if ($errors->any)
-        <div class="alert alert-danger">
-            <h6>Corregir los siguientes errores</h6>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
+    <div class="car">
+        <h2 class="card-header">Crear usuario</h2>
+        <div class="card-body">
+
+                @if ($errors->any())
+                <div class="alert alert-danger">
+                    <h6>Corregir los siguientes errores</h6>
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif
+
+                <form method="POST" action="{{ route('users.crear') }}">
+                    {{ csrf_field() }}
+
+                    <div class="form-group">
+                            <label for="name">Nombre:</label>
+                            <input type="text" class="form-control" name="name" id="name" placeholder="Nombre Apellido" value="{{ old('name')}}">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="email">Correo:</label>
+                        <input type="email" class="form-control" name="email" id="email" placeholder="algo@algo.com" value="{{ old('email')}}">
+                    </div>
+
+                    <div class="form-group">
+                    <label for="password">Contraseña:</label>
+                    <input type="password" class="form-control" name="password" id="password" placeholder="Mas de 6 caracteres">
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Crear usuario</button>
+                    <a href="{{ route('users') }}" class="btn btn-link">Regresar al listado de usuarios</a>
+                </form>
         </div>
-        @endif
+    </div>
 
-        <form method="POST" action="{{ route('users.crear') }}">
-            {{ csrf_field() }}
 
-            <label for="name">Nombre:</label>
-            <input type="text" name="name" id="name" placeholder="Nombre Apellido" value="{{ old('name')}}">
-            {{-- @if ($errors->has('name'))
-                <p>{{$errors->first('name')}}</p>
-            @endif --}}
-<br>
-            <label for="email">Correo:</label>
-            <input type="email" name="email" id="email" placeholder="algo@algo.com" value="{{ old('email')}}">
-            {{-- @if ($errors->has('email'))
-                <p>{{$errors->first('email')}}</p>
-            @endif --}}
-<br>
-            <label for="password">Contraseña:</label>
-            <input type="password" name="password" id="password" placeholder="Mas de 6 caracteres">
-            {{-- @if ($errors->has('password'))
-                <p>{{$errors->first('password')}}</p>
-            @endif --}}
-            <br>
-
-            <button type="submit">Crear usuario</button>
-        </form>
-
-     <p>
-        <a href="{{ route('users') }}">Regresar al listado de usuarios</a>
-    </p>
 
 @endsection
