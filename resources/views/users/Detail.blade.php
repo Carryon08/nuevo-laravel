a6ede50a6f458cd933e4fd6b85026fb557317df8
Roberto <roberto.carreon@cceo.com.mx>
2018-12-26 12:25:20 -0600

    Agregar enlaces a los modulos

--- resources/views/users/Detail.blade.php
+++ resources/views/users/Detail.blade.php
@@ -8,7 +8,14 @@
         <h3>Nombre del usuario: {{ $user->name }}</h3>
         <h2>Correo electronico: {{ $user->email}} </h2>
      <p>
-        <a href="{{ route('users') }}">Regresar al listado de usuarios</a>
+        <a href="{{ route('users') }}">Regresar al listado de usuarios</a> 
+        <br>
+        <a href="{{ route('users.edit', $user)}}">Editar</a>
+        <form action="{{ route('users.destroy', $user)}}" method="POST">
+            {{ method_field('DELETE') }}
+            {{ csrf_field() }}
+            <button type="submit">Eliminar</button>
+        </form>
     </p>
 
 @endsection
